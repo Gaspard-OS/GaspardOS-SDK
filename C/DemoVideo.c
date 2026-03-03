c48ad0d533864a821b69caeea8c78c314de2a7d3
Gaspard <Gaspard@gaspard.com>
2026-03-03 09:29:06 +0100

    add demo video

---
+++ C/DemoVideo.c
@@ -0,0 +1,13 @@
+#include "gaspardos_user_x86.h"
+
+
+
+
+void gmain() {
+
+
+    gaspardapi_init_video(); // init BGA qemu only
+    gaspardapi_demo_3d(); // a very slow and idiomatic sample of raterization
+
+
+}
